1249af9c74a85bbf37c5892218aea737b16bfd8e
Jimmy_dingo <you@example.com>
2020-08-10 20:03:19 +0300

    Added style and replay button

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Board from './Board';
 
 class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
         </div>
-      );
-    }
+          <button className="replay-btn" onClick={() => {
+            window.location.reload(false);
+          }}>Play again!</button>
+      </div>
+    );
   }
+}
 
-  export default Game;
+export default Game;
